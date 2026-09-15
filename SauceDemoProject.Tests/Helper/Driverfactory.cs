a115271ceb379d64c50001e0f7d10930aa88dc4f
Yasmin <thoyasnat@gmail.com>
2026-09-16 11:40:29 +1200

    Update Driver factor helper class to remove WebDriverManager package

--- SauceDemoProject.Tests/Helper/Driverfactory.cs
+++ SauceDemoProject.Tests/Helper/Driverfactory.cs
@@ -1,7 +1,5 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using WebDriverManager;
-using WebDriverManager.DriverConfigs.Impl;
 
 namespace SauceDemoProject.Tests.Helper
 {
@@ -11,28 +9,24 @@ namespace SauceDemoProject.Tests.Helper
         {
             var settings = Config.Settings;
 
-           // new DriverManager().SetUpDriver(new ChromeConfig());
-
             var options = new ChromeOptions();
             if (settings.Headless)
             {
-                options.AddArgument("--headless=new"); //Runs chrome in headless mode
+                options.AddArgument("--headless=new");
             }
-            options.AddArgument("--disable-notifications"); // disables notifications
+            options.AddArgument("--disable-notifications");
             options.AddArgument("--no-sandbox");
-            options.AddArgument("--disable-dev-shm-usage"); // overcome limited temprory memory problems
-            options.AddArgument("--disable-gpu"); // disables GPU acceleration
+            options.AddArgument("--disable-dev-shm-usage");
+            options.AddArgument("--disable-gpu");
             options.AddArgument("--window-size=1920,1080");
 
-            //IWebDriver driver = new ChromeDriver(options);
             var service = ChromeDriverService.CreateDefaultService();
- 
             service.EnableVerboseLogging = true;
             service.LogPath = "chromedriver.log";
 
             IWebDriver driver = new ChromeDriver(service, options);
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(settings.ImplicitWaitSeconds);
-           // driver.Manage().Window.Maximize();
+            driver.Manage().Window.Maximize();
 
             return driver;
         }
